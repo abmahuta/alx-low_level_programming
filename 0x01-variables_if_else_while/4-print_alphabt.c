ad41d2fbd74d7ea2b57a6d5c62ffbf357c8d92d4
Abdulkadir <mahuta2012@gmail.com>
2022-06-04 13:53:29 -0700

    Incomplete alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+for (i = 97; i < 123; i++)
+{
+if (i == 101)
+{
+	continue;
+}
+if (i == 113)
+{
+	continue;
+}
+	putchar(i);
+}
+	putchar ('\n');
+
+	return (0);
+}
