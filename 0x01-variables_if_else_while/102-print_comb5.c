d5623f0c3adda6c68b56c5ac2e797210514b164c
Abdulkadir <mahuta2012@gmail.com>
2022-06-06 10:48:36 -0700

    102-comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 	putchar(32);
 	putchar((num2 / 10) + '0');
 	putchar((num2 % 10) + '0');
-if (num1 == 98  && num2 == 99)
+	if (num1 == 98  && num2 == 99)
 	continue;
 	putchar(44);
 	putchar(32);
