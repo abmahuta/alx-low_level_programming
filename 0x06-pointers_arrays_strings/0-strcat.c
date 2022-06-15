e4c4034880dce3038bf7b624ccfce6fdca01c0f3
Abdulkadir <mahuta2012@gmail.com>
2022-06-15 10:21:31 -0700

    Concatenated string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenate a string to another string
+ *
+ * Return: cont
+ * @dest: Destination string
+ * @src: Source string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char dest[] = "Hello,";
+	char src[] = "World!";
+	char cont;
+
+	cont = _strcat strn1 strn2;
+
+	return (cont);
+}
