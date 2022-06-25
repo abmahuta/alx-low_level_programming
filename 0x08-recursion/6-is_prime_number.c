a805301549a5982b8dfb4d9b7287a35a184b7b69
Abdulkadir <mahuta2012@gmail.com>
2022-06-25 15:07:43 -0700

    Task 6 update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * check_prime - Check all number < n if they can divide it
+ * @n: An int
+ * @resp: An int
+ * Return: An int
+ */
+
+int check_prime(int n, int resp)
+{
+	if (resp >= n && n > 1
+	return (1);
+	else if (n % resp == 0 || n <= 1)
+	return (0);
+	else
+	return (check_prime(n, resp + 1));
+}
+
+/**
  * is_prime_number - Checks if n is a prime number
  * @n: A given number
  * Return: 1 if the number is prime or 0 otherwise
@@ -10,22 +27,4 @@
 int is_prime_number(int n)
 {
 	return (check_prime(n, 2));
-}
-
-/**
- * check_prime - check all number < n if they can divide it
- * @n: int
- * @resp: int
- * Return: int
- */
-
-int check_prime(int n, int resp)
-{
-
-	if (resp >= n && n > 1)
-		return (1);
-	else if (n % resp == 0 || n <= 1)
-		return (0);
-	else
-		return (check_prime(n, resp + 1));
 }
